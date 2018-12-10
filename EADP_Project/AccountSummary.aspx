33ab435ac5e1b59f184f3694baddb873c99b61f9
zennichi <YunxShino.dev@gmail.com>
2018-12-10 13:51:29 +0800

    Added Account Summary Page and Draft Email Service Files
    Account Summary Page added. Email Service file is added but work in progress.

---
+++ EADP_Project/AccountSummary.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="AccountSummary.aspx.cs" Inherits="EADP_Project.AccountSummary" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="col-lg-12" style="margin-top:25px">
+                <div class="card">
+                    <div class="card-header"><h4>Account Summary</h4></div>
+                    <div class="card-body">
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Mail Test" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</asp:Content>
---
+++ EADP_Project/Entities/mailService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+//currently not working
+namespace EADP_Project.Entities
+{
+
+    public class mailService
+    {
+        private const string SMTP_EMAIL = "username";
+        private const string SMTP_PASSWORD = "password";
+
+
+        public void sendmail()
+        {
+                MailMessage mailmessage = new MailMessage("zzenzen2461@gmail.com", "zzenzen2461@gmail.com");
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                mailmessage.Subject = "Exception";
+                smtpClient.Credentials = new System.Net.NetworkCredential("zzenzen2461@gmail.com", "zenzen24^!");
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mailmessage);
+            
+        }
+    }
+}
