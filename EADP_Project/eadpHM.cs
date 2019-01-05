08530552741def48e0cfe2a00e83959ece26b261
ZzsoulesszZ <45791870+ZzsoulesszZ@users.noreply.github.com>
2019-01-05 14:31:31 +0800

    Implemented Standard HttpModule & Logging
    HttpModule implemented at a basic level.
    Added 2 files: eadpHM.cs and logger.txt
    eadpHM.cs contains code for httpmodule [to be copy and pasted around]
    logger.txt logs down user navigation around webpage

---
+++ EADP_Project/eadpHM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace EADP_Project.App_Start
+{
+    public class eadpHM : IHttpModule
+    {
+        private StreamWriter sw;
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += (new EventHandler(this.Application_BeginRequest));
+        }
+
+        private void Application_BeginRequest(Object source, EventArgs e)
+        {
+            if (!File.Exists("logger.txt"))
+            {
+                sw = new StreamWriter(@"C:\Users\Justin Tan\Documents\GitHub\ASP_Project\EADP_Project\logger.txt");
+            }
+            else
+            {
+                sw = File.AppendText("logger.txt");
+            }
+            sw.WriteLine("User sends request at {0}", DateTime.Now);
+            sw.Close();
+        }
+    }
+}
